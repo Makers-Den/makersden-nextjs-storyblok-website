9c04af01da9ac6c457e22254a848081558961c41
mateuszziomekmakers <mateusz.ziomek@makersden.io>
2025-03-05 14:55:54 +0100

    chore: remove StoryblokProvider

--- src/components/storyblok/StoryblokProvider.tsx
+++
@@ -1,9 +0,0 @@
-// 'use client';
-
-// import { getStoryblokApi } from "@/storyblok";
-// import { ReactNode } from "react";
-
-// export default function StoryblokProvider({ children }: { children: ReactNode }) {
-//   getStoryblokApi(); // Re-initialize on the client
-//   return children;
-// }
